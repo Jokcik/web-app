a00a3c87a3f54f4256fbe4a6f6a94d26d9378ac6
jokci <jokcik@yandex.ru>
2017-10-13 15:10:16 +0300

    add nodeserver

---
+++ devserver.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+
+var app = express();
+
+app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, 'static')));
+
+app.get('*', (req, res) => {
+  res.sendFile('index.html', {root: 'dist'});
+});
+
+app.listen(3000);
+
