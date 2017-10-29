79d7a18e5339dd630f3626a8c8a0885f797c3d09
jokci <jokcik@yandex.ru>
2017-10-29 16:41:54 +0300

    add content contacts

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import {ODCommonModule} from '../core/od-common.module';
 import {ContactsRoutingModule} from './contacts-routing.module';
 import {ContactsComponent} from './contacts.component';
+import {CommonModule} from '@angular/common';
+import {MatButtonModule, MatInputModule} from '@angular/material';
 
 @NgModule({
   imports: [
     ContactsRoutingModule,
-    ODCommonModule
+    CommonModule,
+    ODCommonModule,
+    MatButtonModule,
+    MatInputModule
   ],
   declarations: [
     ContactsComponent,
