1aab2cc83b0cd9179f1765f52ec307d273e91cd4
jokcik <jokcik@yandex.ru>
2017-12-01 08:47:59 +0300

    fix --prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  port: 443,
   host: 'https://frozen-headland-84421.herokuapp.com/api/'
 };
--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,10 @@ import * as express from 'express';
 import * as serveStatic from 'serve-static';
 import * as cors from 'express-cors';
 import * as nconf from 'nconf'
+import {environment} from './environments/environment.prod';
 
 let s = express();
-s.set('port', process.env.PORT || nconf.get('port') || nconf.get('PORT') || 3001);
+s.set('port', environment.port || process.env.PORT || nconf.get('port') || nconf.get('PORT') || 3001);
 
 async function bootstrap() {
 
