758fb705a089461fa2f349c89f57aeca29579217
jokcik <jokcik@yandex.ru>
2017-12-06 21:30:36 +0300

    add schools

--- src/app/schools/schools.component.ts
+++ src/app/schools/schools.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DshiEditService} from '../admin/edit/dshi-edit/dshi-edit.service';
+import {Schools} from '../admin/edit/shared/school';
+import {Region} from '../admin/edit/shared/region';
+import {RegionService} from '../admin/edit/region/region.service';
+import {combineLatest} from 'rxjs/observable/combineLatest';
 
 @Component({
   selector: 'od-schools',
   templateUrl: './schools.component.html'
 })
 export class SchoolsComponent implements OnInit {
+  public regions: Region[] = [];
+  public schools: Schools[] = [];
 
-  constructor() { }
+  public currentRegionId: string;
+  public currentSchools: Schools[] = [];
 
-  ngOnInit() {
+  constructor(private schoolsService: DshiEditService,
+              private regionService: RegionService) {
   }
 
+  ngOnInit() {
+    combineLatest(
+      this.regionService.query().$observable,
+      this.schoolsService.query().$observable
+    )
+      .subscribe(([regions, schools]) => {
+        this.schools = schools;
+        this.regions = regions;
+        this.updateSchools(regions[0]._id);
+      }
+    );
+  }
+
+  public updateSchools(id: string) {
+    this.currentRegionId = id;
+    this.currentSchools = this.schools.filter(school => school.region._id == this.currentRegionId);
+  }
 }
--- src/app/schools/schools.module.ts
+++ src/app/schools/schools.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import {SchoolsRoutingModule} from './schools-routing.module';
 import {ODCommonModule} from '../core/od-common.module';
 import {SchoolsComponent} from './schools.component';
+import {MatCardModule, MatFormFieldModule, MatOptionModule, MatSelectModule} from '@angular/material';
+import {DshiEditService} from '../admin/edit/dshi-edit/dshi-edit.service';
+import {RegionService} from '../admin/edit/region/region.service';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   imports: [
     SchoolsRoutingModule,
-    ODCommonModule
+    ODCommonModule,
+    CommonModule,
+    FormsModule,
+
+    MatCardModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatOptionModule
   ],
   declarations: [
     SchoolsComponent,
   ],
-  providers: []
+  providers: [DshiEditService, RegionService]
 })
 export class SchoolsModule { }
