42613f1ad6abced7fa913e019ba0339a820dd4f5
jokcik <jokcik@yandex.ru>
2018-01-07 21:02:32 +0300

    change config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  host: 'https://frozen-headland-84421.herokuapp.com/api/'
+  // host: 'https://frozen-headland-84421.herokuapp.com/api/'
+  host: 'http://85.143.175.134/api/'
 };
