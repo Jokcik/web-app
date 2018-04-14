adb13db942c7e698df0b607f4d6316411a019f52
danil <jokcik@yandex.ru>
2018-04-14 08:26:52 +0300

    fix bug teacher

--- src/app/admin/edit/teacher/teacher.component.ts
+++ src/app/admin/edit/teacher/teacher.component.ts
@@ -48,10 +48,10 @@ export class TeacherComponent implements OnInit {
   public addTeacher() {
     if (this.type === 0) {
       this.teacherService.save(this.teacher).$observable.subscribe(teacher => {
-          this.snackBar.open('Преподаватель успешно добавлен', 'ОК', {duration: 2000});
-          this.teacher = teacher;
-        }, err => window.alert('Ошибка ' + JSON.stringify(err))
-      );
+        const name = this.teacher.name + ' ' + this.teacher.suname + ' ' + this.teacher.middleName;
+        this.snackBar.open(`Преподаватель '${name}' успешно добавлен`, 'ОК', {duration: 4000});
+        this.teacher = new Teacher();
+      }, err => window.alert('Ошибка ' + JSON.stringify(err)));
       return;
     }
 
